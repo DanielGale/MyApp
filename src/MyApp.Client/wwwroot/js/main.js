88e74e07eb5af941d8bd4c08c9089ffb45ce3f6c
Daniel Gale <daniel.gale@gmail.com>
2020-03-10 13:44:23 -0400

    Add Bearer token to client

--- src/MyApp.Client/wwwroot/js/main.js
+++ src/MyApp.Client/wwwroot/js/main.js
@@ -6,6 +6,22 @@ new Vue({
         states: []
     },
     mounted() {
+        axios.interceptors.request.use(
+            (config) => {
+                let token = localStorage.getItem('authtoken');
+
+                if (token) {
+                    config.headers['Authorization'] = `Bearer ${token}`;
+                }
+
+                return config;
+            },
+
+            (error) => {
+                return Promise.reject(error);
+            }
+        );
+
         axios.get('https://localhost:5001/api/states')
             .then(response => this.states = response.data);
     }
